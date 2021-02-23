97acc8b992919771661cce79874217be2eff8220
gbcm18 <32985438+gbcm18@users.noreply.github.com>
2021-02-23 14:36:07 +0300

    Update game.py

--- game.py
+++ game.py
@@ -19,10 +19,11 @@ blue = (0, 0, 255)
 file_0 = 'music.wav'
 pygame.mixer.init()
 pygame.mixer.music.load(file_0)
-pygame.mixer.music.play(-1)
+#pygame.mixer.music.play(-1)
+width = 962
+height = 600
 
-
-win = pygame.display.set_mode((962,600))
+win = pygame.display.set_mode([width,height])
 pygame.display.set_caption("Medieval Game")
 
 bg = pygame.image.load("oyun_arka_planim.png")
@@ -54,6 +55,8 @@ class Player(object):
         self.right = False
         self.walkCount = 0
         self.jumpCount = 10
+        self.hitbox = (self.x + 20, self.y + 20, 50,90)
+
 
     def draw(self, win):
 
@@ -77,13 +80,45 @@ class Player(object):
                 win.blit(chars["char"][1], (self.x,self.y))
             else:
                 win.blit(chars["char"][1], (self.x,self.y))
+        self.hitbox = (self.x + 20, self.y + 20, 50, 90)
+        hitbox = pygame.draw.rect(win, (255, 255, 255), self.hitbox, 1)
+        if self.y < 450 and self.isJump == False and new_scene == False:
+            self.y += 10
 
-def draw_3(win):
-     basamak_taban = pygame.draw.rect(win, (88, 0, 0), [0, 540, 962, 60])
-     basamak1 = pygame.draw.rect(win, (0,0,0),[385,465,117,75])
-     basamak2 = pygame.draw.rect(win, (0, 0, 0), [494, 390, 120, 80])
-     basamak3 = pygame.draw.rect(win, (0, 0, 0), [591,308,120,84])
 
+    def draw_3(self, win):
+         hitbox = pygame.draw.rect(win, (255, 255, 255), self.hitbox, 1)
+         basamak_taban = pygame.draw.rect(win, (88, 0, 0), [0, 540, 962, 60])
+         first_rect = pygame.draw.rect(win, (0, 0, 0), [385, 465, 117, 75])
+         second_rect = pygame.draw.rect(win, (0,0,0), [494, 390, 120, 80])
+         ucuncu_rect = pygame.draw.rect(win, (0,0,0), [596,308,117,84])
+         dorduncu_rect = pygame.draw.rect(win, (0, 0, 0), [701,240,270,70])
+
+
+         
+         if hitbox.colliderect(first_rect):
+             if abs(first_rect.left - hitbox.right) < 10:
+                 self.x -= 10
+             elif abs(first_rect.top - hitbox.bottom) < 10:
+                 self.y = 314
+         if hitbox.colliderect(second_rect):
+             if abs(second_rect.left - hitbox.right) < 10:
+                 self.x -= 5
+             elif abs(second_rect.top - hitbox.bottom) < 60:
+                 self.y -= 70
+                 #self.y -= 70
+         if hitbox.colliderect(ucuncu_rect):
+             if abs(ucuncu_rect.left - hitbox.right) < 10:
+                 self.x -= 5
+             elif abs(ucuncu_rect.top - hitbox.bottom) < 55:
+                 self.y -= 41
+         if hitbox.colliderect(dorduncu_rect):
+             if abs(dorduncu_rect.left - hitbox.right) < 10:
+                 self.x -= 5
+             elif abs(dorduncu_rect.top - hitbox.bottom) < 55:
+                 self.y -= 35
+         elif (self.y < 450) and (self.isJump == False):
+             self.y += 10
 
 
 
@@ -103,25 +138,28 @@ class GameKey(object):
             #pygame.quit()
 
 new_scene = False
+
 def redrawGameWindow():
     if (new_scene == True):
         win.blit(bg3, (0,0))
-        draw_3(win)
+        man.draw_3(win)
         man.draw(win)
 
     else:
         win.blit(bg, (0,0))
         man.draw(win)
+
     yellow_key.draw2(win)
     pygame.display.update()
 
-man = Player(300, 450, 100,120)
+#man = Player(300, 450, 100,120)
+man = Player(300,200,100,120)
 yellow_key = GameKey(50, 450, 40, 43)
 
 
 walking = True
 while (walking):
-    clock.tick(27)
+    clock.tick(40)
 
 
     keys = pygame.key.get_pressed()
@@ -135,12 +173,12 @@ while (walking):
             exit()
 
 
-    if keys[pygame.K_LEFT] and (man.x > man.vel):
+    if (keys[pygame.K_LEFT] or keys[pygame.K_a]) and (man.x > man.vel):
         man.x -= man.vel
         man.left = True
         man.right = False
         man.standing = False
-    elif keys[pygame.K_RIGHT] and (man.x < 962 - man.width - man.vel):
+    elif (keys[pygame.K_RIGHT] or keys[pygame.K_d]) and (man.x < 962 - man.width - man.vel):
         man.x += man.vel
         man.right = True
         man.left = False
@@ -157,11 +195,10 @@ while (walking):
         new_scene = True
         man.x = 50
         man.y = 450
-
         #pygame.mixer.music.stop()
 
     if not(man.isJump):
-        if keys[pygame.K_UP]:
+        if (keys[pygame.K_UP] or keys[pygame.K_w]):
             man.isJump = True
             man.right = False
             man.left = False
@@ -176,6 +213,7 @@ while (walking):
         else:
             man.isJump = False
             man.jumpCount = 10
+    #print(man.x,man.y)
 
     redrawGameWindow()
 
